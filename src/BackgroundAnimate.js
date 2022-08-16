eac3f251608e25cba529115e3d52baa756386150
nishita-20 <ngnishita00@gmail.com>
2022-08-16 15:52:47 +0530

    My Project

---
+++ src/BackgroundAnimate.js
@@ -0,0 +1,22 @@
+const BackgroundAnimate = () => {
+  return (
+    <div className="hero">
+    <ul>
+      <li className="cube"></li>
+      <li className="cube"></li>
+      <li className="cube"></li>
+      <li className="cube"></li>
+      <li className="cube"></li>
+      <li className="cube"></li>
+      <li className="cube"></li>
+      <li className="cube"></li>
+      <li className="cube"></li>
+      <li className="cube"></li>
+      
+      
+    </ul>
+    </div>
+  )
+}
+
+export default BackgroundAnimate
